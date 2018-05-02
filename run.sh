5f3ee4bb1cc0ecf3d30e793d45ba629639c1114f
Alex Gray <agray@clearcareonline.com>
2018-05-02 10:34:43 -0400

    Become region aware

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "generating aws_config.json..."
+REGION=`curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+echo "Region is: $REGION"
+cat > aws_config.json <<EOF
+  {
+    "region": "$REGION"
+  }
+EOF
+echo "starting c3vis..."
+npm start
