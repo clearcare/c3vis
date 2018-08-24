99fb8e2d1d6faa7bb11c8243015bd30f155332d1
Alex Gray <agray@clearcareonline.com>
2018-08-24 13:02:37 -0400

    use AWS_REGION env var

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "generating aws_config.json..."
-REGION=`curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+export REGION=`curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+export AWS_REGION=$REGION
 echo "Region is: $REGION"
 cat > aws_config.json <<EOF
   {
